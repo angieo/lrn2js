735aa5296d43fb006f327882d9f7fa694c908eed
Angelika Olejniczak <anesqo@gmail.com>
2014-05-15 09:08:36 +0200

    Fixing love

--- web.js
+++ web.js
@@ -1,21 +1,24 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+//var express = require("express");
+//var logfmt = require("logfmt");
+//var app = express();
 var http = require('http'),
     url = require('url'),
     fs = require('fs');
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-
-fs.readFile('./index.html', function(err, data) {
-    res.end(data);
-  });
-  //res.send('I LOVE YOU SHRI');
-});
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+http.createServer(function (req, res) {
+   // parse URL
+   var url_parts = url.parse(req.url);
+   console.log(url_parts);
+   if(url_parts.pathname == '/') {
+      // file serving
+      fs.readFile('./index.html', function(err, data) {
+         res.end(data);
+      });
+   } else if(url_parts.pathname.substr(0, 5) == '/poll') {
+     // polling code here
+  }
+}).listen(port, 'localhost');
+console.log('Server running.');
