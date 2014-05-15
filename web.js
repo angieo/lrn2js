3f756c13c633925ebdd5063f9aa6c9016ede7c87
Angelika Olejniczak <anesqo@gmail.com>
2014-05-15 09:12:11 +0200

    Fixing love again

--- web.js
+++ web.js
@@ -12,13 +12,11 @@ http.createServer(function (req, res) {
    // parse URL
    var url_parts = url.parse(req.url);
    console.log(url_parts);
-   if(url_parts.pathname == '/') {
+   //if(url_parts.pathname == '/') {
       // file serving
       fs.readFile('./index.html', function(err, data) {
          res.end(data);
       });
-   } else if(url_parts.pathname.substr(0, 5) == '/poll') {
-     // polling code here
-  }
+   //}
 }).listen(port, 'localhost');
 console.log('Server running.');
