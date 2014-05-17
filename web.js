f296e62da6e78da2ea290a45162e9872286d3b10
Angelika Olejniczak <anesqo@gmail.com>
2014-05-17 14:41:17 +0200

    fixing port issue

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080);
+app.listen(port);
