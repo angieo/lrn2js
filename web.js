b2f52a41acd5d56fe1c7e79caf8b2ae0f1b2cc35
Angelika Olejniczak <anesqo@gmail.com>
2014-05-17 13:37:37 +0200

    working server

--- web.js
+++ web.js
@@ -1,22 +1,6 @@
-//var express = require("express");
-//var logfmt = require("logfmt");
-//var app = express();
-var http = require('http'),
-    url = require('url'),
-    fs = require('fs');
+var express = require('express'),
+    app = express();
 
+app.use(express.static(__dirname + '/public'));
 
-var port = Number(process.env.PORT || 5000);
-
-http.createServer(function (req, res) {
-   // parse URL
-   var url_parts = url.parse(req.url);
-   console.log(url_parts);
-   //if(url_parts.pathname == '/') {
-      // file serving
-      fs.readFile('./index.html', function(err, data) {
-         res.end(data);
-      });
-   //}
-}).listen(port, 'localhost');
-console.log('Server running.');
+app.listen(8080);
